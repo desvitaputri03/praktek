8b46e8b71fe195111ea60d8bc0b94d1f405b0d74
Desvita Putri <161610929+desvitaputri03@users.noreply.github.com>
2024-06-08 23:48:28 +0700

    Add files via upload

---
+++ PBO_VITA/src/Desvita1705/Arithmetic.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Desvita1705;
+
+/**
+ *
+ * @author HP
+ */
+class Arithmetic extends Exception {
+    
+}
